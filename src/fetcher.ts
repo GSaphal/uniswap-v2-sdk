53fe72db2be1c946dabfb60a3a12b82c5bf352b2
Saphal Ghimire <gsaphal99@gmail.com>
2021-08-26 10:23:56 +0545

    Adding Fetching Token Name and Address From Contract

--- src/fetcher.ts
+++ src/fetcher.ts
@@ -52,7 +52,10 @@ export abstract class Fetcher {
             }
             return decimals
           })
-    return new Token(chainId, address, parsedDecimals, symbol, name)
+    name = await new Contract(address, ERC20, provider).name()
+    symbol = await new Contract(address, ERC20, provider).symbol()
+
+    return new Token(chainId, address, parsedDecimals, name, symbol)
   }
 
   /**
